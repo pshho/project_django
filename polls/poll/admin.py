aca2df3d53207e9f9780738d93bd80f04d97c7d0
pshho <128444203+pshho@users.noreply.github.com>
2023-06-21 00:08:57 +0900

    v6

--- polls/poll/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from poll.models import Question, Choice
-
-# Question, Choice 클래스 관리자 페이지에 등록
-admin.site.register(Question)
-admin.site.register(Choice)
--- pyweb/board/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from board.models import Question, Answer
-
-admin.site.register(Question)
-admin.site.register(Answer)
